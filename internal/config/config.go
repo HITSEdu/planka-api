ab11556b54f20643f9c494fac52834fccb9f20c7
YungOleg <bilk5899@gmail.com>
2026-06-20 23:29:24 +0700

    fix: cors

--- internal/config/config.go
+++ internal/config/config.go
@@ -57,6 +57,7 @@ func Load() (Config, error) {
 			"http://localhost:5173",
 			"http://127.0.0.1:3000",
 			"http://127.0.0.1:5173",
+			"https://planka-web.vercel.app"
 		}),
 		AccessTokenTTL:  accessTokenTTL,
 		RefreshTokenTTL: refreshTokenTTL,
